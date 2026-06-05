0cad258f3268260c9050853eeeb734d6972caba9
kacper <corgodev@gmail.com>
2026-06-05 12:10:03 +0200

    feat: add nix flake for dev tools management

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "corgo.dev development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+
+          shellHook = ''
+            echo "🚀 corgo.dev dev environment loaded"
+            echo "Node.js $(node -v)"
+            echo "NPM $(npm -v)"
+          '';
+        };
+      });
+}
